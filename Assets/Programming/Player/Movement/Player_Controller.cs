262e34174411b8e7d9814fe15077616c07c52b80
Mickname342 <michele.marchese.andreu@gmail.com>
2024-09-18 17:31:37 +0100

    Basic Movement done
    Basic Movement done

---
+++ Assets/Programming/Player/Movement/Player_Controller.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Controller : MonoBehaviour
+{
+    [SerializeField] private Rigidbody _rb;
+    [SerializeField] private float _speed = 5;
+    [SerializeField] private float _turnSpeed = 360;
+    private Vector3 _input;
+    Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+    private void Update()
+    {
+        GatherInput();
+        Look();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void GatherInput()
+    {
+        _input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+    }
+
+    private void Look()
+    {
+        if (_input == Vector3.zero) return;
+
+        var rot = Quaternion.LookRotation(_input.ToIso(), Vector3.up);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, rot, _turnSpeed * Time.deltaTime);
+    }
+
+    private void Move()
+    {
+        //_rb.MovePosition(transform.position + transform.forward * _input.normalized.magnitude * _speed * Time.deltaTime);
+        if (_input != Vector3.zero)
+        {
+            animator.SetFloat("Running", 1);
+        }
+        else if (_input == Vector3.zero)
+        {
+            animator.SetFloat("Running", 0);
+        }
+    }
+}
+

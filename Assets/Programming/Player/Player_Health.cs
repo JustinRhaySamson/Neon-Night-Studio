4862fd05a87faef3b359fca5bbd96df55d82f596
Mickname342 <michele.marchese.andreu@gmail.com>
2024-10-14 12:51:53 +0100

    Start time stop
    Start time stop

--- Assets/Programming/Player/Player_Health.cs
+++ Assets/Programming/Player/Player_Health.cs
@@ -2,15 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 
 public class Player_Health : MonoBehaviour
 {
     public int HP = 2;
+    GameObject UI_Text;
+    TMP_Text UI_HP;
+
+    private void Start()
+    {
+        UI_Text = GameObject.Find("HP number");
+        UI_HP = UI_Text.GetComponent<TMP_Text>();
+        UI_HP.text = HP.ToString();
+    }
 
     public void Get_Hit()
     {
         HP--;
-        print("Player HP: " + HP);
+        UI_HP.text = HP.ToString();
         if (HP <= 0)
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);

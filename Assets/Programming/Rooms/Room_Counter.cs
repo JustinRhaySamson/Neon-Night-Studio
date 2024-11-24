775202f244851b38830a193b8c5b3c71438ea9a2
Mickname342 <michele.marchese.andreu@gmail.com>
2024-11-24 18:38:58 +0000

    Started Room counters
    Started Room counters

--- Assets/Programming/Rooms/Room_Counter.cs
+++ Assets/Programming/Rooms/Room_Counter.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Room_Counter : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] int enemies_killed = 0;
     void Start()
     {
         
@@ -14,5 +14,10 @@ public class Room_Counter : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void EnemyKilled()
+    {
+        enemies_killed++;
     }
 }

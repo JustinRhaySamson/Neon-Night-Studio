7bfdb7206415cc2ee9d83a24a84fcca805e4be93
Mickname342 <michele.marchese.andreu@gmail.com>
2024-11-24 19:03:32 +0000

    Enemy kill counter works
    Enemy kill counter works

--- Assets/Programming/Rooms/Room_Counter.cs
+++ Assets/Programming/Rooms/Room_Counter.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class Room_Counter : MonoBehaviour
 {
     [SerializeField] int enemies_killed = 0;
+
+    public int[] requirements;
+    public GameObject[] doors;
+
+    int requirements_met = 0;
     void Start()
     {
         
@@ -19,5 +24,16 @@ public class Room_Counter : MonoBehaviour
     public void EnemyKilled()
     {
         enemies_killed++;
+
+        if (enemies_killed == requirements[requirements_met])
+        {
+            Open_Doors();
+            requirements_met++;
+        }
+    }
+
+    void Open_Doors()
+    {
+        Destroy(doors[requirements_met]);
     }
 }

68d71bec2481dd550bd529aea88b217d15935da3
Mickname342 <73951374+Mickname342@users.noreply.github.com>
2025-02-22 20:07:10 +0000

    Added placeholder pause menu
    Added placeholder pause menu

--- Assets/Programming/UI/Show_Controls.cs
+++ Assets/Programming/UI/Show_Controls.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 
 public class Show_Controls : MonoBehaviour
@@ -10,10 +12,14 @@ public class Show_Controls : MonoBehaviour
     bool active = false;
     public GameObject reset_button;
     bool reset_active = false;
+    public GameObject resume_button;
+    public GameObject pause_menu;
+    public RectTransform arrow;
     void Start()
     {
         controls.SetActive(false);
         reset_button.SetActive(false);
+        Time.timeScale = 1;
     }
 
     // Update is called once per frame
@@ -23,7 +29,17 @@ public class Show_Controls : MonoBehaviour
         {
             print("I pressed Menu");
             active = !active;
-            controls.SetActive(active);
+            pause_menu.SetActive(active);
+            if (active)
+            {
+                Time.timeScale = 0f;
+            }
+            else if (!active)
+            {
+                Time.timeScale = 1;
+            }
+            var eventSystem = EventSystem.current;
+            eventSystem.SetSelectedGameObject(resume_button, new BaseEventData(eventSystem));
         }
     }
 
@@ -34,5 +50,16 @@ public class Show_Controls : MonoBehaviour
             reset_active = !reset_active;
             reset_button.SetActive(reset_active);
         }
+
+        var eventSystem = EventSystem.current;
+        arrow.position = eventSystem.currentSelectedGameObject.transform.position;
+
+    }
+
+    public void Resume_Button()
+    {
+        active = !active;
+        pause_menu.SetActive(active);
+        Time.timeScale = 1;
     }
 }

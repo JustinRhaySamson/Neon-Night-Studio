8937c1809765265d0021beef0bdd2a514fd30010
Mickname342 <73951374+Mickname342@users.noreply.github.com>
2025-03-02 18:01:21 +0000

    Working wave system
    Working wave system

---
+++ Assets/Programming/Rooms/Wave_Starter.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wave_Starter : MonoBehaviour
+{
+    public GameObject[] enemies_to_spawn;
+    public int enemies_to_kill;
+    Wave_System wave_system;
+    // Start is called before the first frame update
+    void Start()
+    {
+        wave_system = GameObject.Find("Wave Manager").GetComponent<Wave_System>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") || other.CompareTag("Invincible"))
+        {
+            wave_system.Select_Enemies(enemies_to_spawn,enemies_to_kill);
+        }
+    }
+}

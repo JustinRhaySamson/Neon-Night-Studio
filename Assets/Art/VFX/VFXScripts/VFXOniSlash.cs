e375a767675cc06b02a9011578a984941e253ff7
Bombprooft <99894562+Bombprooft@users.noreply.github.com>
2025-03-16 06:05:25 +0000

    VFX stuff

---
+++ Assets/Art/VFX/VFXScripts/VFXOniSlash.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VFXOniSlash : MonoBehaviour
+{
+    public GameObject VFXA1;
+
+    public void ActivateA1()
+    {
+        VFXA1.SetActive(true);
+        //Debug.Log("VFX activated");
+    }
+
+    public void DeactivateA1()
+    {
+        VFXA1.SetActive(false);
+        //Debug.Log("VFX deactivated");
+    }
+}
